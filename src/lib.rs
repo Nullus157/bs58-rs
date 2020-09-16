a3fa2ffa80aebe2111201fc88c64cc5f209715ba
Wim Looman <git@nemo157.com>
2020-09-16 22:17:33 +0200

    Replace unwrap by ? in examples

--- src/lib.rs
+++ src/lib.rs
@@ -42,9 +42,10 @@
 //! ## Basic example
 //!
 //! ```rust
-//! let decoded = bs58::decode("he11owor1d").into_vec().unwrap();
+//! let decoded = bs58::decode("he11owor1d").into_vec()?;
 //! let encoded = bs58::encode(decoded).into_string();
 //! assert_eq!("he11owor1d", encoded);
+//! # Ok::<(), bs58::decode::Error>(())
 //! ```
 //!
 //! ## Changing the alphabet
@@ -52,21 +53,22 @@
 //! ```rust
 //! let decoded = bs58::decode("he11owor1d")
 //!     .with_alphabet(bs58::alphabet::RIPPLE)
-//!     .into_vec()
-//!     .unwrap();
+//!     .into_vec()?;
 //! let encoded = bs58::encode(decoded)
 //!     .with_alphabet(bs58::alphabet::FLICKR)
 //!     .into_string();
 //! assert_eq!("4DSSNaN1SC", encoded);
+//! # Ok::<(), bs58::decode::Error>(())
 //! ```
 //!
 //! ## Decoding into an existing buffer
 //!
 //! ```rust
 //! let (mut decoded, mut encoded) = ([0xFF; 8], String::with_capacity(10));
-//! bs58::decode("he11owor1d").into(&mut decoded).unwrap();
+//! bs58::decode("he11owor1d").into(&mut decoded)?;
 //! bs58::encode(decoded).into(&mut encoded);
 //! assert_eq!("he11owor1d", encoded);
+//! # Ok::<(), bs58::decode::Error>(())
 //! ```
 
 #[cfg(feature = "std")]
@@ -102,7 +104,8 @@ enum Check {
 /// ```rust
 /// assert_eq!(
 ///     vec![0x04, 0x30, 0x5e, 0x2b, 0x24, 0x73, 0xf0, 0x58],
-///     bs58::decode("he11owor1d").into_vec().unwrap());
+///     bs58::decode("he11owor1d").into_vec()?);
+/// # Ok::<(), bs58::decode::Error>(())
 /// ```
 ///
 /// ## Changing the alphabet
@@ -112,17 +115,19 @@ enum Check {
 ///     vec![0x60, 0x65, 0xe7, 0x9b, 0xba, 0x2f, 0x78],
 ///     bs58::decode("he11owor1d")
 ///         .with_alphabet(bs58::alphabet::RIPPLE)
-///         .into_vec().unwrap());
+///         .into_vec()?);
+/// # Ok::<(), bs58::decode::Error>(())
 /// ```
 ///
 /// ## Decoding into an existing buffer
 ///
 /// ```rust
 /// let mut output = [0xFF; 10];
-/// assert_eq!(8, bs58::decode("he11owor1d").into(&mut output).unwrap());
+/// assert_eq!(8, bs58::decode("he11owor1d").into(&mut output)?);
 /// assert_eq!(
 ///     [0x04, 0x30, 0x5e, 0x2b, 0x24, 0x73, 0xf0, 0x58, 0xFF, 0xFF],
 ///     output);
+/// # Ok::<(), bs58::decode::Error>(())
 /// ```
 ///
 /// ## Errors
